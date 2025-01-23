5c4f758f7d99080149477d61765695c08e56d7fa
HtmlIsBetterThanC <170306799+HtmlIsBetterThanC@users.noreply.github.com>
2025-01-23 18:44:44 +0100

    feat: added downloadAudio to cardRepository

--- app/src/main/java/task/languagecard/data/repository/card/CardRepository.kt
+++ app/src/main/java/task/languagecard/data/repository/card/CardRepository.kt
@@ -1,9 +1,12 @@
 package task.languagecard.data.repository.card
 
 import task.languagecard.model.network.NetworkCard
+import java.io.File
 
 interface CardRepository {
     suspend fun getAllCard(): List<NetworkCard>
 
     suspend fun getCard(id: Int): NetworkCard?
+
+    suspend fun downloadAudio(outputDir: File)
 }
--- app/src/main/java/task/languagecard/data/repository/card/CardRepositoryImpl.kt
+++ app/src/main/java/task/languagecard/data/repository/card/CardRepositoryImpl.kt
@@ -2,12 +2,17 @@ package task.languagecard.data.repository.card
 
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsChannel
 import io.ktor.client.statement.readRawBytes
+import io.ktor.utils.io.jvm.javaio.toInputStream
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 import kotlinx.serialization.json.Json
 import task.languagecard.di.IoDispatcher
 import task.languagecard.model.network.NetworkCard
+import java.io.File
+import java.io.FileOutputStream
+import java.util.zip.ZipInputStream
 import javax.inject.Inject
 
 class CardRepositoryImpl @Inject constructor(
@@ -26,6 +31,32 @@ class CardRepositoryImpl @Inject constructor(
         return withContext(ioDispatcher) {
             val list = getAllCard()
             return@withContext list.find { it.id == id }
+        }
+    }
+
+    override suspend fun downloadAudio(outputDir: File) {
+        withContext(ioDispatcher) {
+            if (outputDir.listFiles()?.isEmpty() == true) {
+                val response = httpClient.get("audio/curated_platform_audios/sm1_new_kap1.zip")
+                val channel = response.bodyAsChannel()
+                val inputStream = channel.toInputStream()
+
+                ZipInputStream(inputStream).use { zipInputStream ->
+                    var entry = zipInputStream.nextEntry
+                    while (entry != null) {
+                        val filePath = "$outputDir/${entry.name}"
+                        if (entry.isDirectory) {
+                            File(filePath).mkdirs()
+                        } else {
+                            FileOutputStream(filePath).use {
+                                zipInputStream.copyTo(it)
+                            }
+                        }
+                        zipInputStream.closeEntry()
+                        entry = zipInputStream.nextEntry
+                    }
+                }
+            }
         }
     }
 }
